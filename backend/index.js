9bd6f9a85eb7df7ea540d3c781af96035d1f440e
Phan Huu Phuoc Loc <109939299+locphp@users.noreply.github.com>
2025-03-25 08:37:40 +0700

    feat: add database.js and update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
-const express = require('express')
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors')
+const productRouter = require('./routes/product.router');
+
+const connection = require("./database/database")
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.use(express.static('public'));
+app.use(cors());
+
+app.use('/api/products', productRouter);
+
+(async () => {
+    try {
+        await connection();
+        app.listen(port, () => {
+            console.log(`Backend listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log(">>> Error connect to DB:", error)
+    }
+
+})()
