ed7c8d2ca273d3f64ace5ad2ee5afa25c9090677
Phan Huu Phuoc Loc <109939299+locphp@users.noreply.github.com>
2025-04-15 09:39:30 +0700

    reset main

--- frontend/src/helper/index.js
+++
@@ -1,7 +0,0 @@
-
-export function truncateDescription(description, maxLength) {
-    if (description.length > maxLength) {
-      return description.slice(0, maxLength) + '...';
-    }
-    return description;
-  }  
