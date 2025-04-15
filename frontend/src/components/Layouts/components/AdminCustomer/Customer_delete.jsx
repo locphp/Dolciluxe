bd3cbe81a146a5b20c9e9a8baaa3a6e9490fa7e9
Phan Huu Phuoc Loc <109939299+locphp@users.noreply.github.com>
2025-04-15 09:47:31 +0700

    reset main (#32)

--- frontend/src/components/Layouts/components/AdminCustomer/Customer_delete.jsx
+++
@@ -1,7 +0,0 @@
-import { getListUsers, deleteUsers } from "~/api/apiUser";
-import React ,{ useEffect,useState } from "react";
-
-function DeleteUsers(){
-
-}
-export default DeleteUsers;
--- frontend/src/redux/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-export const selectCartItems = (state) => state.cart.list;
-
-export const selectCartTotal = createSelector(
-  [selectCartItems],
-  (list) =>
-    list.reduce((acc, item) => acc + item?.buy_quantity * item?.price, 0)
-);
