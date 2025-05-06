b6c31f16ad4c494886b813d56c83d465ec3d8011
ManGia44 <vienthongphuhung1@gmail.com>
2025-05-06 08:42:00 +0700

    #82 - fix success payment ver1

--- frontend/src/components/Layouts/components/Payment/PaymentSucess.jsx
+++ frontend/src/components/Layouts/components/Payment/PaymentSucess.jsx
@@ -3,32 +3,32 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { Result, Button, message } from 'antd';
 
 const PaymentSuccess = () => {
-  const location = useLocation();
+  // const location = useLocation();
   const navigate = useNavigate();
-  const query = new URLSearchParams(location.search);
-  const token = query.get('token');
+  // const query = new URLSearchParams(location.search);
+  // const token = query.get('token');
 
-  useEffect(() => {
-    const authData = JSON.parse(localStorage.getItem('paymentAuth'));
+  // useEffect(() => {
+  //   const authData = JSON.parse(localStorage.getItem('paymentAuth'));
 
-    // Kiểm tra token hợp lệ
-    const isValid = (
-      authData &&
-      authData.token === token &&
-      authData.expires > Date.now()
-    );
+  //   // Kiểm tra token hợp lệ
+  //   const isValid = (
+  //     authData &&
+  //     authData.token === token &&
+  //     authData.expires > Date.now()
+  //   );
 
-    if (!isValid) {
-      navigate('/', { replace: true });
-      message.error('Token không hợp lệ hoặc đã hết hạn!');
-    } else {
-      // Xóa token sau khi xác thực
-      localStorage.removeItem('paymentAuth');
-    }
-  }, [token, navigate]);
+  //   if (!isValid) {
+  //     navigate('/', { replace: true });
+  //     message.error('Token không hợp lệ hoặc đã hết hạn!');
+  //   } else {
+  //     // Xóa token sau khi xác thực
+  //     localStorage.removeItem('paymentAuth');
+  //   }
+  // }, [token, navigate]);
 
   return (
-    <div className="payment-success-page">
+    <div className="payment-success-page my-16 ml-12 flex flex-col items-center justify-center text-center">
       {/* Nội dung trang thành công */}
       <Result
         status="success"
